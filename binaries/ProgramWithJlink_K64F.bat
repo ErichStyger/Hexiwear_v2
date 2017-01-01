28e443e95ce0a6d6622a9f8170ff9dc5a494db8a
Erich Styger <erich.styger@hslu.ch>
2017-01-01 13:51:52 +0100

    added binary files and programming routines

---
+++ binaries/ProgramWithJlink_K64F.bat
@@ -0,0 +1,8 @@
+REM *******************************************************
+REM * Batch file to program a bin file with Segger J-Link *
+REM *******************************************************
+
+SET JLINK="C:\Program Files (x86)\SEGGER\JLink_V612d\JLink.exe"
+
+%JLINK% -device MK64FN1M0xxx12 -CommanderScript ./jlink_k64f.script
+pause
---
+++ binaries/readme.txt
@@ -0,0 +1,6 @@
+readme.txt
+----------
+Script to program the Hexiwear binary firmware files.
+
+See
+https://mcuoneclipse.com/2016/12/07/flashing-and-restoring-the-hexiwear-firmware/
